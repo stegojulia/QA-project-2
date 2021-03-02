0dc2b1cf900a1cde636e146ad73ee9bfe2213c40
stegojulia <j.z.jablonska@gmail.com>
2021-03-02 07:43:21 +0000

    Interface A changes

--- service-1/app.py
+++ service-1/app.py
@@ -5,7 +5,6 @@ import random
 import pymysql
 import mysql.connector
 import datetime
-import sqlite3
 import requests
 from sqlalchemy.ext.automap import automap_base
 import requests
@@ -39,7 +38,7 @@ def home():
     review_data = Review(word=word.text, revision_date=datetime.datetime.now())
     db.session.add(review_data)
     db.session.commit()
-    version = 'version B'
+    version = 'version A'
     return render_template('home.html', word=word.text, sentence=sentence.text, learning=learning.text, version=version)
 
 if __name__ == "__main__":
--- service-1/tests/test_unit_service1.py
+++ service-1/tests/test_unit_service1.py
@@ -44,8 +44,9 @@ class TestResponse(TestBase):
             with patch("app.get_sentence") as p1:
                 with patch("app.get_learning") as p2:
                     g.return_value.text = "amor"
-                    p1.return_value.text = "No le gustan las historias de amor."
-                    p2.return_value.text = "New word: review in 2 days"
+                    p1.return_value.text = "love"
+                    p2.return_value.text = "New word"
                     version = 'version B'
                     response = self.client.get(url_for("home"))
-                    self.assertIn(b'<!DOCTYPE html>\n<html>\n\n    <body style="background-color:powderblue;">\n    <body>\n        <p>version B</p>\n        <h1>Your word:</h1>\n        <h2>amor</h2>\n\n        <h1>Translation:</h1>\n        <h2>No le gustan las historias de amor.</h2>\n\n        <h1>To review:</h1>\n        <h2>New word: review in 2 days</h2>\n    </body>\n\n\n\n\n\n</html>', response.data)
+                    self.assertIn(b'<!DOCTYPE html>\n<html>\n    <body>\n        <p>version A</p>\n        <h1>Your word:</h1>\n        <h2>amor</h2>\n\n        <h1>Your sentence:</h1>\n        <h2>love</h2>\n\n        <h1>Last viewed:</h1>\n        <h2>New word</h2>\n    </body>\n\n\n\n\n\n</html>', response.data)
+
